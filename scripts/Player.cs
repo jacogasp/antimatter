32d1d67af4d41c2fa07ce54ed15e0b70cf38d6e3
Jacopo Gasparetto <jacopo.gasparetto@cnaf.infn.it>
2024-04-04 09:45:37 +0200

    First commit

---
+++ scripts/Player.cs
@@ -0,0 +1,145 @@
+using Algorithms;
+using Godot;
+
+namespace Characters
+{
+  public partial class Player : CharacterBody2D
+  {
+    [Export]
+    public float Speed { get; set; } = 5.0f;
+
+    [Export]
+    public float JumpVelocity { get; set; } = 4.5f;
+
+    [Export]
+    public float AngularSpeed { get; set; } = 1.0f;
+
+    private readonly StateMachine<Player> stateMachine = new();
+    private Vector2 _acceleration = Vector2.Zero;
+
+    // Get the gravity from the project settings to be synced with RigidBody nodes.
+    private readonly float gravity = ProjectSettings
+      .GetSetting("physics/2d/default_gravity")
+      .AsSingle();
+
+    public override void _Ready()
+    {
+      stateMachine.CurrentState = new Idle();
+      GD.Print("Player ready");
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+      stateMachine.Update(this, delta);
+      HandleMovement();
+      AddForce(Vector2.Down * gravity);
+      Velocity += _acceleration * (float)delta;
+      MoveAndSlide();
+      _acceleration *= Vector2.Zero;
+    }
+
+    void AddForce(Vector2 force)
+    {
+      _acceleration += force;
+    }
+
+    public static Vector2 InputDirection
+    {
+      get
+      {
+        return Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+      }
+    }
+
+
+    void HandleMovement()
+    {
+      var target = Position + InputDirection;
+      var desired = target - Position;
+      desired = desired.Normalized() * Speed;
+      var steering = desired - Velocity;
+      AddForce(steering * AngularSpeed);
+    }
+  }
+
+  // States
+
+  class Idle : State<Player>
+  {
+    public override void Enter(Player gameObject)
+    {
+      GD.Print("enter idle");
+    }
+
+    public override State<Player> HandleInput(Player gameObject)
+    {
+      if (Input.IsActionJustPressed("jump") && gameObject.IsOnFloor())
+      {
+        return new Jumping();
+      }
+      if (Player.InputDirection != Vector2.Zero)
+      {
+        return new Running();
+      }
+      return this;
+    }
+
+    public override void Update(Player gameObject, float delta)
+    {
+
+    }
+  }
+
+  class Running : State<Player>
+  {
+    public override void Enter(Player gameObject)
+    {
+      GD.Print("enter running");
+    }
+
+    public override State<Player> HandleInput(Player gameObject)
+    {
+      if (Input.IsActionJustPressed("jump") && gameObject.IsOnFloor())
+      {
+        return new Jumping();
+      }
+      if (Player.InputDirection == Vector2.Zero)
+      {
+        return new Idle();
+      }
+      return this;
+    }
+
+    public override void Update(Player gameObject, float delta)
+    {
+      var velocity = gameObject.Velocity;
+      velocity.X = Player.InputDirection.X * gameObject.Speed;
+      gameObject.Velocity = velocity;
+    }
+  }
+
+  class Jumping : State<Player>
+  {
+    public override void Enter(Player gameObject)
+    {
+      GD.Print("enter jumping");
+      var velocity = gameObject.Velocity;
+      velocity.Y = gameObject.JumpVelocity;
+      gameObject.Velocity = velocity;
+    }
+
+    public override State<Player> HandleInput(Player gameObject)
+    {
+      if (gameObject.IsOnFloor())
+      {
+        return new Idle();
+      }
+      return this;
+    }
+
+    public override void Update(Player gameObject, float delta)
+    {
+
+    }
+  }
+}
---
+++ scripts/StateMachine.cs
@@ -0,0 +1,31 @@
+
+namespace Algorithms
+{
+  public abstract class State<T>
+  {
+    public abstract void Enter(T gameObject);
+    public abstract State<T> HandleInput(T gameObject);
+    public abstract void Update(T gameObject, float delta);
+  }
+
+  public class StateMachine<T>
+  {
+    public State<T> CurrentState { get; set; }
+
+    public void Update(T gameObject, double delta)
+    {
+      var nextState = CurrentState.HandleInput(gameObject);
+      if (nextState == null)
+      {
+        return;
+      }
+      if (nextState != CurrentState)
+      {
+        nextState.Enter(gameObject);
+      }
+      CurrentState = nextState;
+      CurrentState.HandleInput(gameObject);
+      CurrentState.Update(gameObject, (float)delta);
+    }
+  }
+}
